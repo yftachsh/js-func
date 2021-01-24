3d7cd5ddf9d90d8839b35158ec3f08b20f083c87
Yftach Shmaryahu <yftachshm@gmail.com>
2021-01-24 21:11:25 +0200

    Converted occurenceMap test to use assert

--- tests/occurenceMap.js
+++ tests/occurenceMap.js
@@ -1,12 +1,25 @@
+const assert = require('assert');
 const occurenceMap = require('../arrays/occurenceMap');
 
-const testArray = [
-    1,
-    2,
-    3,
-    4,
-    3,
-    7
-];
+module.exports = () => {
+    const testArray = [
+        1,
+        2,
+        3,
+        4,
+        3,
+        7
+    ];
 
-console.log(occurenceMap(testArray));
+    const expectedMap = {
+        '1': 1,
+        '2': 1,
+        '3': 2,
+        '4': 1,
+        '7': 1
+    }
+
+    const resultMap = occurenceMap(testArray);
+
+    assert.deepStrictEqual(resultMap, expectedMap, 'occurenceMap method failed');
+}
