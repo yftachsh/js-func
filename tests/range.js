7c73a8ce71b54f328b4ae3e2d01883c65a7c5340
Yftach Shmaryahu <yftachshm@gmail.com>
2021-01-24 21:15:08 +0200

    Converted range test to use assert

--- tests/range.js
+++ tests/range.js
@@ -1,3 +1,12 @@
+const assert = require('assert');
 const range = require('../arrays/range');
 
-console.log(range(5, 50));
+module.exports = () => {
+    const start = 5;
+    const end = 10;
+
+    const expectedRange = [5, 6, 7, 8, 9, 10];
+
+    const rangeResult = range(start, end);
+    assert.deepStrictEqual(rangeResult, expectedRange, 'Failed to range numbers');
+}
